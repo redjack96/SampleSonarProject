a9f73ca6ccd0b15d992e53b1ad9ffaec402d953c
Giacomo <giacomo.redjack@gmail.com>
2021-03-06 18:25:13 +0100

    Ignorata cartella sonarscan

--- src/main/java/org/giacomo/rossi/HelloSonar.java
+++ src/main/java/org/giacomo/rossi/HelloSonar.java
@@ -2,7 +2,7 @@ package org.giacomo.rossi;
 
 public class HelloSonar {
     public static void main(String[] args) {
-        System.out.println("Print da IntelliJ");
-        System.out.println("Print da Eclipse");
+        System.out.println("Print da IntelliJ 1");
+        System.out.println("Print da Eclipse 1");
     }
 }
