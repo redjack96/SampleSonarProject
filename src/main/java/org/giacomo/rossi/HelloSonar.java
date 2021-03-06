14d41db159af7cdce06d3259ce8d57b5f3d6ce43
Giacomo <giacomo.redjack@gmail.com>
2021-03-06 17:05:04 +0100

    Corretto gitignore eclipse e modificato HelloSonar

--- src/main/java/org/giacomo/rossi/HelloSonar.java
+++ src/main/java/org/giacomo/rossi/HelloSonar.java
@@ -3,5 +3,6 @@ package org.giacomo.rossi;
 public class HelloSonar {
     public static void main(String[] args) {
         System.out.println("Print da IntelliJ");
+        System.out.println("Print da Eclipse");
     }
 }
