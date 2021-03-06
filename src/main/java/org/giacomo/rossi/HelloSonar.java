3377c2932e3e8c1e45834428fb2739cb93a3afb2
Giacomo <giacomo.redjack@gmail.com>
2021-03-06 18:53:13 +0100

    Tentativo 2 per analisi dopo push

--- src/main/java/org/giacomo/rossi/HelloSonar.java
+++ src/main/java/org/giacomo/rossi/HelloSonar.java
@@ -3,6 +3,6 @@ package org.giacomo.rossi;
 public class HelloSonar {
     public static void main(String[] args) {
         System.out.println("Print da IntelliJ: Me lo analizzi? Grazie!");
-        System.out.println("Print da Eclipse 1");
+        System.out.println("Print da Eclipse 1: ora ho tolto l'analisi automatica!");
     }
 }
