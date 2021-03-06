3ce7844a9ddae7f17cfdd10aab741ae891e9c71d
Giacomo <giacomo.redjack@gmail.com>
2021-03-06 19:28:48 +0100

    Aggiunto Logger

--- src/main/java/org/giacomo/rossi/HelloSonar.java
+++ src/main/java/org/giacomo/rossi/HelloSonar.java
@@ -2,7 +2,8 @@ package org.giacomo.rossi;
 
 public class HelloSonar {
     public static void main(String[] args) {
-        System.out.println("Print da IntelliJ: Me lo analizzi? Grazie!");
-        System.out.println("Print da Eclipse 1: ora ho tolto l'analisi automatica!");
+        MyLogger.logInfo("Print da IntelliJ: Me lo analizzi? Grazie!");
+        MyLogger.logWarning("Print da Eclipse 1: ora ho tolto l'analisi automatica!");
+        MyLogger.logSevere("Autodistruzione tra 3 2 1 ...");
     }
 }
---
+++ src/main/java/org/giacomo/rossi/MyLogger.java
@@ -0,0 +1,26 @@
+package org.giacomo.rossi;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class MyLogger {
+
+    private static final String LOGGER_NAME = "MyLogger";
+    private static final Logger instance = Logger.getLogger(LOGGER_NAME);
+
+    private MyLogger(){
+        instance.setLevel(Level.ALL);
+    }
+
+    public static void logInfo(String message){
+        instance.log(Level.INFO, message);
+    }
+
+    public static void logWarning(String message){
+        instance.log(Level.WARNING, message);
+    }
+
+    public static void logSevere(String message){
+        instance.log(Level.SEVERE, message);
+    }
+}
