93fb8f40f77d25efab04aa997c097667f55e3fd3
Giacomo <giacomo.redjack@gmail.com>
2021-03-06 18:46:22 +0100

    Aggiunta dipendenza per SonarCloud auto-analisi

--- src/main/java/org/giacomo/rossi/HelloSonar.java
+++ src/main/java/org/giacomo/rossi/HelloSonar.java
@@ -2,7 +2,7 @@ package org.giacomo.rossi;
 
 public class HelloSonar {
     public static void main(String[] args) {
-        System.out.println("Print da IntelliJ 1");
+        System.out.println("Print da IntelliJ: Me lo analizzi? Grazie!");
         System.out.println("Print da Eclipse 1");
     }
 }
